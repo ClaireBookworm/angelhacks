5053948c931a19d7933457e7e8516f97e2ce8b8c
hacker719 <neel.redkar@gmail.com>
2019-07-24 23:06:46 -0700

    Update index.html and remove others
    - Update the index.html completely
    - remove unesesary CSS
    - Take out sponsor button Bc we dont have a patreon

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-function application {
-    document.getElementById("application").innerHTML="Thanks for applying!"
+function application() {
+    document.getElementById("application").innerHTML = "Thanks for applying!"
 }
+(function () {
+    var burger = document.querySelector('#nav-toggle');
+    var menu = document.querySelector('.navbar-menu');
+    burger.addEventListener('click', function () {
+        burger.classList.toggle('is-active');
+        menu.classList.toggle('is-active');
+    });
+})();
+var scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 450
+});
