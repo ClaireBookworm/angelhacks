eefee0315c8b080e5833c9fa12ac2f78f69ce273
Neel Redkar <neel.redkar@outlook.com>
2019-07-29 17:42:12 -0700

    add captcha

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function application() {
 var scroll = new SmoothScroll('a[href*="#"]', {
     speed: 450
 });
-$("#submit").click(function () {
+document.getElementById("form").addEventListener("submit",(e)=> {
 	$.post("https://AngelHacks-Reg--hacker22.repl.co/addperson",
   	{
     "First-Name":document.getElementById("fname").value,
@@ -20,15 +20,22 @@ $("#submit").click(function () {
        "School":document.getElementById("school").value,
 	"Gender":document.getElementById("gender").value,
 	"other":document.getElementById("comment").value,
-	"email":document.getElementById("email").value
+  "email":document.getElementById("email").value,
+  "captcha":document.getElementById("g-recaptcha-response").value
   },
   function (data) {
+    if (data.success == 0) {
+      alert("Please check the reCaptcha box")
+    }
 	document.getElementById("fname").value = "";
 	  document.getElementById("lname").value = "";
 	document.getElementById("school").value = "";
 	document.getElementById("comment").value = "";
   	document.getElementById("email").value = "";
   });
+});
+$("#submit").click(function () {
+
 })
 
 settings = {
