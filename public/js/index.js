69703713b10c7b2c8ce15b3ed4e9bfd7fc12ab36
Ken Mueller <kenmueller0@gmail.com>
2019-08-01 16:22:21 -0700

    working on sign up

--- public/js/index.js
+++ public/js/index.js
@@ -7,45 +7,62 @@ document.addEventListener('DOMContentLoaded', () => {
 	const signUpSchoolInput = document.querySelector('section.hero.sign-up form.sign-up .school-input')
 	const signUpPasswordInput = document.querySelector('section.hero.sign-up form.sign-up .password-input')
 	const signUpCommentsInput = document.querySelector('section.hero.sign-up form.sign-up .comments-input')
+	const signUpButton = document.querySelector('section.hero.sign-up form.sign-up .submit')
+	const signInEmailInput = document.querySelector('.abc') // Fix
+	const signInPasswordInput = document.querySelector('.abc') // Fix
+	const signInButton = document.querySelector('.abc') // Fix
+
+	let isSignUp = false
 
 	auth.onAuthStateChanged(user =>
 		!isSignedIn && user
 			? firestore.doc(`users/${user.uid}`).get().then(doc => {
 				if (doc.exists) {
-					setLoading(signUpButton, false)
-					alert(`There is already a user with the email ${emailInput.value}`)
-				} else
-					firestore.doc(`users/${user.uid}`).set({
-						name: nameInput.value,
-						email: emailInput.value
-					}).then(() => {
-						setLoading(signUpButton, false)
-						setCookie('uid', user.uid)
-						if (fromAction && fromUrl)
-							close()
-						else
-							location.href = searchParams.get('from') || '/'
-					}).catch(_error => {
-						setLoading(signUpButton, false)
+					if (isSignUp) {
+						setLoading(signUpButton)(false)
+						alert(`There is already a user with the email ${signUpEmailInput.value}`)
+					} else {
+						setLoading(signInButton)(false)
+						setCookie('uid')(user.uid)
+						location.reload()
+					}
+				} else if (isSignUp)
+					firestore.doc(`users/${user.uid}`).set(Object.assign({
+						name: signUpNameInput.value,
+						email: signUpEmailInput.value,
+						school: signUpSchoolInput.value
+					}, signUpCommentsInput.value.length ? { comments: signUpCommentsInput.value } : {})).then(() => {
+						setLoading(signUpButton)(false)
+						setCookie('uid')(user.uid)
+						location.reload()
+					}).catch(() => {
+						setLoading(signUpButton)(false)
 						alert('There was a problem creating an account. Please try again')
 					})
+				else {
+					setLoading(signInButton)(false)
+					alert(`A user already exists with email ${user.email}`)
+				}
 			})
 			: null
 	)
 
-	const setSignUpButtonsLoading = loading =>
-		
-
 	const signUp = () => {
-		
+		isSignUp = true
+		setLoading(signUpButton)(true)
+		const email = signUpEmailInput.value
+		auth.createUserWithEmailAndPassword(email, signUpPasswordInput.value).catch(() => {
+			setLoading(signUpButton)(false)
+			alert(`There is already a user with the email ${email}`)
+		})
 	}
 
 	const signUpFieldsDidChange = () =>
-		document.querySelector('section.hero.sign-up form.sign-up .submit').disabled = !(
+		signUpButton.disabled = !(
 			signUpNameInput.value.length &&
 			signUpEmailInput.value.length &&
 			signUpSchoolInput.value.length &&
-			signUpCommentsInput.value.length > 5
+			signUpPasswordInput.value.length > 5
 		)
 	
 	const addSignUpFieldsDidChangeListener = element =>
