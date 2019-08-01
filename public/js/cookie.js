d2d3069dcb77a9634165801eaf90fe76252aeb0c
Ken Mueller <kenmueller0@gmail.com>
2019-08-01 15:54:08 -0700

    const functions for cookie.js

--- public/js/cookie.js
+++ public/js/cookie.js
@@ -1,12 +1,10 @@
-function cookie(name) {
+const cookie = name => {
 	const cookies = document.cookie.match(`(^|[^;]+)\\s*${name}\\s*=\\s*([^;]+)`)
 	return cookies ? cookies.pop() : undefined
 }
 
-function setCookie(name, value) {
+const setCookie = name => value =>
 	document.cookie = `${name}=${value}; expires=Thu, 01 Jan 3000 00:00:00 GMT`
-}
 
-function removeCookie(name) {
+const removeCookie = name =>
 	document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 GMT`
-}
