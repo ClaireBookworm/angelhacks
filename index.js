49133ced2ee18e873878e9de36bf4322d26e2753
Neel Redkar <neel.redkar@outlook.com>
2019-08-17 23:24:27 -0700

    add static back

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-function application() {
-    document.getElementById("application").innerHTML = "Thanks for applying!"
-}
+
 (function () {
     var burger = document.querySelector('#nav-toggle');
     var menu = document.querySelector('.navbar-menu');
@@ -12,33 +10,6 @@ function application() {
 var scroll = new SmoothScroll('a[href*="#"]', {
     speed: 450
 });
-document.getElementById("form").addEventListener("submit",(e)=> {
-  e.preventDefault();
-	$.post("https://angelhacksreg.now.sh/addperson",
-  	{
-    "First-Name":document.getElementById("fname").value,
-	"Last-Name":document.getElementById("lname").value,
-       "School":document.getElementById("school").value,
-	"Gender":document.getElementById("gender").value,
-	"other":document.getElementById("comment").value,
-  "email":document.getElementById("email").value,
-  "captcha":document.getElementById("g-recaptcha-response").value
-  },
-  function (data) {
-    if (data.success == 0) {
-      alert("Please check the reCaptcha box")
-    }
-	document.getElementById("fname").value = "";
-	  document.getElementById("lname").value = "";
-	document.getElementById("school").value = "";
-	document.getElementById("comment").value = "";
-  	document.getElementById("email").value = "";
-  });
-});
-$("#submit").click(function () {
-
-})
-
 settings = {
     "particles": {
       "number": {
